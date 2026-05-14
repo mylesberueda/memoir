e9a57fbf2ebbf4a8de8eb4ccf551582b587c7907
Myles Berueda <myles@polypixel.io>
2026-05-13 19:21:52 -1000

    chore: gen entities

--- apps/memoir-service/src/models/_entity/prelude.rs
+++ apps/memoir-service/src/models/_entity/prelude.rs
@@ -2,4 +2,5 @@
 
 pub use super::api_keys::Entity as ApiKeys;
 pub use super::bootstrap_tokens::Entity as BootstrapTokens;
+pub use super::memories::Entity as Memories;
 pub use super::users::Entity as Users;
